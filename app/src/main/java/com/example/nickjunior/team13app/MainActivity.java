c6eaba16916e663d594a073a3a326b957691d21a
Nick Van Acker <nick.vanacker@student.ap.be>
2018-12-20 15:48:37 +0100

    TestCommit

--- app/src/main/java/com/example/nickjunior/team13app/MainActivity.java
+++ app/src/main/java/com/example/nickjunior/team13app/MainActivity.java
@@ -6,10 +6,13 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private boolean test13 = false;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
